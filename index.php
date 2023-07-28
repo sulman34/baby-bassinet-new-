077a8131b11c8cde483445d82ece7bc56033dd7f
sulman34 <136338808+sulman34@users.noreply.github.com>
2023-07-28 09:31:10 -0700

    index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title><?php echo "Best Baby Bassinet of 2023"; ?></title>
+</head>
+<body>
+    <h1 style="margin-right: 0in; margin-left: 0in; font-size: 32px; font-family: 'Times New Roman', serif; font-weight: bold; text-align: center;">
+        <span style="font-size: 35px;">Best <span style="color: rgb(44, 130, 201); background-color: rgb(239, 239, 239);">
+                <a href="http://www.pickbassinet.com">Baby Bassinet</a>
+            </span> of 2023</span>
+    </h1>
+    <p style="margin-right: 0in; margin-left: 0in; font-size: 16px; font-family: 'Calibri', sans-serif; margin-top: 0in; margin-bottom: 8.0pt; font-size: 11.0pt; text-align: center; line-height: normal;">
+        <span style="font-size: 19px; font-family: 'Times New Roman', serif;">A great place for parents looking for a safe &amp; comfortable sleeping environment for their baby.&nbsp;</span>
+    </p>
+    <p style="margin-right: 0in; margin-left: 0in; font-size: 16px; font-family: 'Calibri', sans-serif; margin-top: 0in; margin-bottom: 8.0pt; font-size: 11.0pt; text-align: center; line-height: normal;">
+        <span style="font-size: 19px; font-family: 'Times New Roman', serif; color: black;">Best baby bassinet 2023<strong>&nbsp;</strong>is a great choice that seeks the attention of parents. It is a small bed supported on a frame that may or may not be portable and provides neonates a perfect space to sleep</span>
+    </p>
+    <!-- Other sections of the HTML content -->
+    <?php
+    // You can add more PHP logic here if needed.
+    ?>
+</body>
+</html>
